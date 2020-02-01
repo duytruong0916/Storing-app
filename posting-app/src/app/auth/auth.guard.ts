e2074b858837c6f052571b98660b584871184f24
Duy Truong <duytruong169@gmail.com>
2020-02-01 14:01:52 -0600

    new

--- posting-app/src/app/auth/auth.guard.ts
+++ posting-app/src/app/auth/auth.guard.ts
@@ -5,13 +5,14 @@ import {Observable} from 'rxjs'
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGaurd implements CanActivate{
+export class AuthGaurd implements CanActivate {
   constructor(private authservice: AuthService,
               private router: Router){}
   canActivate(
    route: ActivatedRouteSnapshot,
    state: RouterStateSnapshot):
-   boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean |UrlTree> {
+   boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean |UrlTree>
+   {
      const isAuth = this.authservice.getIsAuthenticated();
      if(!isAuth)
      {
